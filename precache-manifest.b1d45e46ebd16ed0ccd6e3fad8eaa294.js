66007cc370f6910b5752a31cb4e31ef349f5c56a
David Balayan <davidb@redkite.io>
2022-04-18 15:09:21 +0400

    Updates

---
+++ precache-manifest.b1d45e46ebd16ed0ccd6e3fad8eaa294.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3179e7a58681932cb757b42fd8cf742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77239a3e27841aba9c14",
+    "url": "/static/css/main.69a7ed2e.chunk.css"
+  },
+  {
+    "revision": "cb170ed7f435afc85587",
+    "url": "/static/js/2.27505fc1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.27505fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77239a3e27841aba9c14",
+    "url": "/static/js/main.74f022fc.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "51437ecf7253dd3bd93620ade79d50cb",
+    "url": "/static/media/logo.51437ecf.svg"
+  },
+  {
+    "revision": "d96274f13ab4f4b7a627e9fb3205ac9d",
+    "url": "/static/media/pasta-1854245.d96274f1.jpg"
+  },
+  {
+    "revision": "3948401c5db9c91e0b3c941873f4310e",
+    "url": "/static/media/sub-buzz-86-1591886919-8.3948401c.jpg"
+  },
+  {
+    "revision": "913877495e55e5742e5c382ffc7cf87e",
+    "url": "/static/media/vegetables-1085063.91387749.jpg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
